ba1388109f3560c6f4dc7bfe18fd117f3b3ca300
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-01-21 22:51:45 +0100

    repro repo

---
+++ App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {useNavigation} from 'react-navigation-hooks';
+
+const NestedStack = createStackNavigator(
+  {
+    NestedHome: () => <Text>Nested</Text>,
+  },
+  {navigationOptions: {headerShown: false}},
+);
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <TouchableOpacity onPress={() => navigation.navigate('Nested')}>
+        <Text>Go to nested</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const RootStack = createStackNavigator({
+  Home: HomeScreen,
+  Nested: NestedStack,
+});
+
+class RootComponent extends React.Component {
+  state = {test: 'not important'};
+
+  render() {
+    return (
+      <RootStack
+        screenProps={{test: 'whatever'}}
+        navigation={this.props.navigation}
+      />
+    );
+  }
+}
+
+RootComponent.router = RootStack.router;
+
+const AppContainer = createAppContainer(RootComponent);
+
+const App = () => {
+  return <AppContainer />;
+};
+
+export default App;
